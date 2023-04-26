d7f6a83b90d1b7e59101e2aac8cee30b86178545
brady-619 <daniel.montano@dish.com.mx>
2023-04-26 14:40:09 -0600

    git commit -m "first commit"

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TodoModule } from './todos/todo.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, TodoModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
